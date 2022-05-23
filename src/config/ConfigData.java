e52022ccbabe0cdcfebc3adcc8efa274619f461a
nguyentancong2 <52781703+nguyentancong2@users.noreply.github.com>
2022-05-23 11:33:58 +0700

    First commit

---
+++ src/config/ConfigData.java
@@ -0,0 +1,14 @@
+package config;
+
+public class ConfigData {
+    public static String DOMAIN = "https://athena.skymavis.one";
+    public static String MAIL_HOST = "pop.gmail.com";
+    public static String MAIL_PORT = "995";
+    public static String IS_ENABLE_TLS = "true";
+    public static String STORE_TYPE = "pop3s";
+    public static String MAIL_USERNAME = "cong.skymavis.demo@gmail.com";
+    public static String MAIL_PASSWORD = "Test@123";
+    public static String MAIL_SENDER = "accounts@axieinfinity.com";
+    public static String MAIL_SUBJECT = "[Axie Infinity] Verify your email";
+
+}
