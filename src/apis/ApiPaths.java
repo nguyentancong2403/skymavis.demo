2ccc5535fe3309526ff277e1cd760979e6c2be32
nguyentancong2403 <105830301+nguyentancong2403@users.noreply.github.com>
2022-05-23 11:29:54 +0700

    Delete src directory

--- src/apis/ApiPaths.java
+++
@@ -1,8 +0,0 @@
-package apis;
-
-public class ApiPaths {
-    public static String REQUEST_VERIFY_EMAIL = "/v1/rpc/auth/request-verify-email";
-    public static String VERIFY_EMAIL = "/v1/rpc/auth/verify-email";
-    public static String REGISTER_WITH_EMAIL = "/v1/rpc/auth/register";
-
-}
